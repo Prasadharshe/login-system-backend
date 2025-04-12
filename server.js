c797e5a9b75d7e244ae9c81565354ae01a68a1fb
Prasadharshe <111993033+Prasadharshe@users.noreply.github.com>
2025-04-12 10:20:51 +0530

    1

--- server.js
+++ server.js
@@ -25,13 +25,9 @@ app.use(express.static(path.join(__dirname, "../login-system-frontend")));
 //     process.exit(1);
 //   });
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Connected to MongoDB Atlas");
+    console.log("✅ Connected to MongoDB Atlas");
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
